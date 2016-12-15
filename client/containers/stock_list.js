9af9f30c163df5d547876153df8a339942f67377
grantmw <grantmwong@gmail.com>
2016-12-14 21:49:53 -0800

    plugged stocks reducer into stock list container, built table with data

--- client/containers/stock_list.js
+++ client/containers/stock_list.js
@@ -1,10 +1,80 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getStock } from '../actions/index'; 
 
-export default class StockList extends Component {
-	render() {
+class StockList extends Component {
+	constructor(props) {
+		super(props);
+		this.renderStock = this.renderStock.bind(this);
+	}
+
+	renderStock(stockData) {
+		const name = stockData.stock.name;
+		const quantity = stockData.quantity;
+		const paidPrice = stockData.stock.bidPrice;
+		const symbol = stockData.stock.symbol;
+
 		return(
-			<div className="stock-list-container">
+			<tr key={name}>
+				<td>
+					<div>
+						{name}
+					</div>
+				</td>
+				<td>
+					<div>
+						{quantity}
+					</div>
+				</td>
+				<td>
+					<div>
+						{paidPrice}
+					</div>
+						<button onClick={() => this.props.getStock(symbol)} className="btn btn-secondary"> View Stock </button>
+				</td>
+			</tr>
+		);
+	}
+
+	render() {
+		if (!this.props.stocks) {
+			return(
+				<div>
+					You Have No Shares to View
+				</div>
+			);
+		}
+		return (
+			<div className="col-md-6 stock-list-container">
+				{this.props.cash}
+				<table className="table table-hover">
+					<thead>
+						<tr>
+							<th>Company</th>
+							<th>Shares</th>
+							<th>Price Paid</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.props.stocks.map(this.renderStock)}
+					</tbody>
+				</table>
 			</div>
 		);
 	}
 };
+
+
+function mapStateToProps(state) {
+	return {
+		stocks: state.stocks,
+		cash: state.cash
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ getStock },dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StockList);
