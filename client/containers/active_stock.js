0521400788439173e1e1fd84e4d0984f6d079f69
grantmw <grantmwong@gmail.com>
2016-12-14 21:00:49 -0800

    activestock info displayed on search

--- client/containers/active_stock.js
+++ client/containers/active_stock.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 
 class ActiveStock extends Component {
+	
+	renderActiveStock() {
+		const stock = this.props.stock
+		const { handleSubmit } = this.props;
+		const quantityValue = this.props.fields.quantityValue;
+
+		return(
+			<div>
+				<h1>{stock.name} ({stock.symbol})</h1>
+				Bid: $ {stock.bidPrice} | Ask: $ {stock.askPrice}
+				<form>
+					<div className="form-group">
+						<input
+							type="text"
+							className="form-control"
+							{...quantityValue}
+							/>
+						<div className="text-help">
+							{quantityValue.touched ? quantityValue.error : ""}
+						</div>
+						<button>Buy</button>
+						<button>Sell</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
 	render() {
 		if (!this.props.stock) {
 			return(
@@ -12,7 +39,7 @@ class ActiveStock extends Component {
 		}
 		return(
 			<div className="col-md-6">
-				{this.props.stock.name}
+				{this.renderActiveStock()}
 			</div>
 		);
 	}
