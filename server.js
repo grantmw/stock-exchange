d34053d2068e701916a4d03ac53dfb82b0b5a109
grantmw <grantmwong@gmail.com>
2016-12-16 15:09:00 -0800

    allow cors

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+app.use(function(req, res, next) { 
+	res.header('Access-Control-Allow-Origin', "*"); 
+	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE'); 
+	res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+	next();
+});
+
 app.get('/api/posts', (req, res) => {
   res.send([{ id: 1 }]);
 });
